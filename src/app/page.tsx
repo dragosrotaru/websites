226d21f36284ba14d709f238a11bad4ef610c269
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-12-08 11:54:49 -0500

    site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,53 +49,12 @@ export default function Home() {
               <h2>Here and Now 🧘</h2>
               <p>
               <strong>
-                  I am seeking great people to do software engineering / ML work
-                  with. I can offer my authentic interest 🧠 a positive attitude
-                  🌞 and commitment to growth 🌱.
+                  I am working on neurosymbolic compilers and program synthesis. If you want to work together,
+                  my email is dragos@rotaru.co. Twitter is @noharamnofoul
                 </strong>
                 </p>
               <p>last update on {LAST_UPDATE}</p>
             </section>
-        </div>
-        <div className={styles.outerSlantUp}>
-          <div className={styles.innerSlantUp}>
-            <section id="hiring">
-              <h2>Lets Work Together</h2>
-              <p>After spending 7 years in the software industry
-                I have decided to return to school so that I can pursue AI research.
-                I am actively looking for work opportunities in conjunction with school.
-                I currently have full availability for work at least for 8 months.
-              </p>
-              <p>
-                Please find{" "}
-                <a href="resume.pdf" target="_blank">
-                  my resume here
-                </a>{" "}
-                for your reading pleasure. I am primarily a Fullstack software engineer
-                 who has dabbled in a wide variety of work,
-                diverse in purpose, scope and technology.
-                My past work includes SaaS B2B platforms 
-                and P2P open source consumer applications
-                for high growth silicon valley companies,
-                web/mobile applications for close to 20 clients,
-                consulting startups on technical direction,
-                finding vulnerabilities in web applications,
-                designing embedded hardware,
-                data analysis for a legal case, and much more.
-                I am also the cofounder/CTO of Huckleberry AI based in Waterloo, we 
-                are working on a better discovery experience for home buyers and have
-                3 products in the market.
-              </p>
-              <p>
-                My workhorse stack is TypeScript, Node, React, AWS and PostGres.
-                I have also worked on production systems in Python, C#, C,
-                Golang, Ruby, Redis, RabbitMQ, Mongo and many other technologies.
-                I am very interested in Haskell, Rust and
-                Lisp variants, and currently I am learning gpu programming with WebGPU.
-              </p>
-            </section>
-            {/* <Portfolio /> */}
-          </div>
         </div>
       </main>
       <footer id="contact">
